cb97f0aecf2aa7a8f19642f090f211dd63d15677
aum47 <aum.patel131108@gmail.com>
2026-05-03 18:55:59 +0530

    Initial Integration of ReSukiSU

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,9 +4,9 @@
 #!/bin/bash
 
 export ARCH=arm64
-export CROSS_COMPILE=../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE=/home/aumpatel/toolchain/gcc-linaro-4.9/bin/aarch64-linux-gnu-
 export ANDROID_MAJOR_VERSION=p
 
 make ARCH=arm64 exynos7870-on7xreflte_defconfig
-make ARCH=arm64 -j64
+make ARCH=arm64 -j64 | tee build.log
 
