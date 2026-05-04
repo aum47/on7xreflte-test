bc72f0d07564ac4360db607d985f20605032c049
aum47 <aum.patel131108@gmail.com>
2026-05-04 10:57:40 +0530

    Streamline build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,12 +1,9 @@
-
-
-
 #!/bin/bash
-
 export ARCH=arm64
 export CROSS_COMPILE=/home/aumpatel/toolchain/gcc-linaro-4.9/bin/aarch64-linux-gnu-
 export ANDROID_MAJOR_VERSION=p
 
-make ARCH=arm64 exynos7870-on7xreflte_defconfig
-make ARCH=arm64 -j64 | tee build.log
+mkdir -p out
 
+make ARCH=arm64 O=out exynos7870-on7xreflte_defconfig
+make ARCH=arm64 O=out -j64 | tee out/build.log
